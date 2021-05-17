9bfacb5f898faa8d785370e8a849b22787bf2bea
Kevin Mei <kevjinx@gmail.com>
2021-05-16 19:53:42 -0700

    organized files

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+
+
+const searchInput = document.getElementById('search-input')
+const searchBtn = document.getElementById('search-btn')
+
+
+
+
+
+
+
+
+
+
+searchBtn.addEventListener('click', () => {
+  //get input
+  //search function
+})
---
+++ scripts/search.js
@@ -0,0 +1,11 @@
+//get search input
+//fetch
+//return data
+
+class Search {
+  constructor(searchInput) {
+    this.searchInput = searchInput;
+  }
+
+
+}
